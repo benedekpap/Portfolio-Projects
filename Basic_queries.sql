18db79b824320372db7fbbfdc0dc97f354600d08
benedekpap <125098211+benedekpap@users.noreply.github.com>
2023-03-01 18:10:24 +0100

    Update and rename Covid_deaths_script.sql to Basic_queries.sql

--- Basic_queries.sql
+++ Basic_queries.sql
@@ -1,10 +1,3 @@
--- Database from: https://ourworldindata.org/covid-deaths
--- Date period: 2020-01-01 - 2023-02-11
--- Data form: CSV file
-
--- During import a small adjustment needed to be made, because MySQL didn't accept the empty values, so I exchanged them using Python Pandas to NULL values.
-
-
 
 -- Basic data check
 SELECT location, date, total_cases, new_cases, total_deaths, population
