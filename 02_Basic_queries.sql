b7fd2847a0b519be04689cda6f7e3bee8eacb9be
benedekpap <125098211+benedekpap@users.noreply.github.com>
2023-03-02 13:34:37 +0100

    Update 02_Basic_queries.sql

--- 02_Basic_queries.sql
+++ 02_Basic_queries.sql
@@ -94,10 +94,10 @@ Where dea.continent is not NULL
 GROUP BY dea.location
 ORDER BY 3 DESC; 
 
--- Checking what percentage of each country is fully vaccinated, partly vaccinated and also the vaccination rate
+-- Checking what percentage of each country is fully vaccinated, partly vaccinated and also the number of total deaths per million
 SELECT dea.location, max(vac.people_fully_vaccinated)/max(dea.population)*100 as 'Fully_Vaccinated',
 max(vac.people_vaccinated)/max(dea.population)*100 as 'Partly_Vaccinated',
-max(vac.total_vaccinations)/max(dea.population)*100 as 'Vaccination_rate'
+max(dea.total_deaths_per_million) as 'Total_deaths_per_million'
 FROM project_portfolio.covid_deaths as dea
 JOIN project_portfolio.covid_vaccinations as vac
 	ON dea.location = vac.location and dea.date = vac.date
@@ -105,7 +105,7 @@ Where dea.continent is not NULL
 GROUP BY dea.location
 ORDER BY 4 DESC; 
 
--- Looking at new vaccinations and total_Vaccinations
+-- Presenting the number of new vaccinations each day by country, and also and number of total vaccinations for each location
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, 
 SUM(vac.new_vaccinations) OVER(PARTITION by dea.location) as 'total_vaccinations'
 FROM project_portfolio.covid_deaths as dea
@@ -114,11 +114,21 @@ JOIN project_portfolio.covid_vaccinations as vac
 Where dea.continent is not NULL
 ORDER BY 6 DESC,3; 
 
--- find the date of the first vaccination
-SELECT dea.location, min(dea.date) as date
+-- Checking the date of the beginning of the vaccinations for each country, and their number of total deaths per million
+SELECT dea.location, min(dea.date) as date, max(dea.total_deaths_per_million) as 'Total_deaths_per_million'
 FROM project_portfolio.covid_deaths as dea
 JOIN project_portfolio.covid_vaccinations as vac
 	ON dea.location = vac.location and dea.date = vac.date
 WHERE vac.new_vaccinations IS NOT NULL and dea.continent is not NULL
 GROUP BY dea.location
 ORDER BY 2;
+
+-- Checking the date of the beginning of the vaccinations for each country, and their number of total cases per million
+SELECT dea.location, min(dea.date) as date, max(dea.total_cases_per_million) as 'Total_cases_per_million'
+FROM project_portfolio.covid_deaths as dea
+JOIN project_portfolio.covid_vaccinations as vac
+	ON dea.location = vac.location and dea.date = vac.date
+WHERE vac.new_vaccinations IS NOT NULL and dea.continent is not NULL
+GROUP BY dea.location
+ORDER BY 2;
+
