ed61d7d9b0bbe5f45b93e5848a575964c63f89fd
benedekpap <125098211+benedekpap@users.noreply.github.com>
2023-03-09 13:18:39 +0100

    Update 03_Creating_views.sql

--- 03_Creating_views.sql
+++ 03_Creating_views.sql
@@ -1,4 +1,4 @@
--- In this section I'll be creating MySQL Views from the Basic_queries file, in order to visualize them later in PowerBI.
+-- In this section I'll be creating MySQL Views mainly from the Basic_queries file, in order to visualize them later in PowerBI.
 
 -- Looking at the vaccination rate and the death rate for each country
 SELECT dea.location, max(vac.total_vaccinations)/max(dea.population) as 'Vaccination_rate', 
@@ -47,3 +47,25 @@ JOIN project_portfolio.covid_vaccinations as vac
 WHERE vac.new_vaccinations IS NOT NULL and dea.continent is not NULL
 GROUP BY dea.location
 ORDER BY 2;
+
+-- Number of cases by continent
+CREATE VIEW CasesByContinent AS 
+SELECT dea.location, max(dea.total_cases) as 'Total_cases', max(dea.total_cases)/max(dea.population) as 'Infection rate' 
+FROM project_portfolio.covid_deaths as dea
+JOIN project_portfolio.covid_vaccinations as vac
+	ON dea.location = vac.location and dea.date = vac.date
+Where dea.continent is NULL AND dea.location not like '%income%' and dea.location not like ('International')
+and dea.location not like 'World' and dea.location not like 'European Union'
+GROUP BY dea.location
+ORDER BY 2 DESC;
+
+-- Countries with the 10 largest death rate
+CREATE VIEW Top10_countries_deathRate AS 
+SELECT dea.location, max(dea.total_deaths)/max(dea.total_cases) as 'Infection rate' 
+FROM project_portfolio.covid_deaths as dea
+JOIN project_portfolio.covid_vaccinations as vac
+	ON dea.location = vac.location and dea.date = vac.date
+Where dea.continent is not NULL 
+GROUP BY dea.location
+ORDER BY 2 DESC
+LIMIT 11;
